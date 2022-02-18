c086043f417affa0a17aa007ddf9d1443990306b
monkey usage <monkeyusage@gmail.com>
2022-02-18 17:55:30 +0100

    try to improve testing

--- test/test.jl
+++ test/test.jl
@@ -8,13 +8,28 @@ function gen_xor_data()
 end
 
 # TODO:
-# modularize model/prediction to be able to pass various architectures
-# implement gradient descent using flux, find appropriate loss function
-# surely have to create some container for both binary weights and float weights (used for back prop)
+# first:
+#   find better test than xor, that one is too easy => find appropriate dataset
+#   implement gradient descent using flux, find appropriate loss function
+# second: surely have to create some container for both binary weights and float weights (used for back prop)
+# last: modularize model/prediction to be able to pass various architectures
+function score_model(weights::Vector{BitArray})::Float64
+    predictions::Vector{Bool}= []
+    ground_truth::Vector{Bool}= []
+    for _ in 1:10
+        x, y = gen_xor_data()
+        γ = model(x, weights)[1]
+        push!(ground_truth, y)
+        push!(predictions, γ)
+    end
+    score = accuracy(predictions, ground_truth)
+    println("acc: $score")
+    return score
+end
 
 function main()
-    ws = [init_weights(2, [2, 3, 1]) for _ in 1:100]
-    xor_model_weights::Vector{BitArray}=[] 
+    ws = [init_weights(2, [2, 3, 1]) for _ in 1:2]
+    xor_model_weights::Vector{BitArray}=[]
     epoch = 1
     while true
         x, y = gen_xor_data()
@@ -30,16 +45,7 @@ function main()
         epoch += 1
     end
 
-    predictions::Vector{Bool}= []
-    ground_truth::Vector{Bool}= []
-    for _ in 1:10
-        x, y = gen_xor_data()
-        γ = model(x, xor_model_weights)[1]
-        push!(ground_truth, y)
-        push!(predictions, γ)
-    end
-
-    println("acc: $(accuracy(predictions, ground_truth))")
+    score_model(xor_model_weights)
 end
 
 main()
