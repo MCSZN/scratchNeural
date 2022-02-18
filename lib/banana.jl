43394479e6e1720bc28b9549da1ad8b2e7e1b284
MCSZN <monkeyusage@gmail.com>
2022-02-18 01:07:58 +0100

    added more tests, including accuracy

--- lib/banana.jl
+++ lib/banana.jl
@@ -2,6 +2,13 @@ using Random
 
 # apply bitwise "and" then "xor" bitarray
 neuron(input::BitVector, weights::BitVector)::Bool = xor.(.&(input, weights)...)
+OutputType = Union{BitVector, Bool}
+
+function init_weights(input_shape::Int, sizes::Vector{Int})::Vector{BitArray}
+    @assert input_shape == sizes[1]
+    @assert length(sizes) >= 2
+    [bitrand(sizes[i], sizes[i+1]) for i in 1:length(sizes)-1]
+end
 
 function layer(inputs, weights)
     # vectorized version of neuron
@@ -11,20 +18,21 @@ function layer(inputs, weights)
     xor.([y[feature, :] for feature in 1:n_features]...)
 end
 
-function init_weights(input_shape::Int, sizes::Vector{Int})::Vector{BitArray}
-    @assert input_shape == sizes[1]
-    @assert length(sizes) >= 2
-    [bitrand(sizes[i], sizes[i+1]) for i in 1:length(sizes)-1]
-end
-
 # input layer: shape ==> (n_features, n_neurons)
 # next layers: shape ==> (n_neurons_prev, n_neurons_next)
-
-function model(x::BitVector, weights::Vector{BitArray})::BitVector
-    out = layer(x, weights[1])
+function model(inputs::BitVector, weights::Vector{BitArray})::OutputType
+    out = layer(inputs, weights[1])
     if length(weights) == 1 return out end
     for weight in weights[2:end]
         out = layer(out, weight)
     end
     out
+end
+
+function predict(inputs::BitVector, weights::Vector{Vector{BitArray}})::Vector{Vector{OutputType}}
+    predictions = Array{BitVector}(undef, length(weights))
+    Threads.@threads for index in 1:length(weights)
+        predictions[index] = model(inputs, weights[index])
+    end
+    predictions
 end
--- test/test.jl
+++ test/test.jl
@@ -9,17 +9,37 @@ end
 
 
 function main()
-    w = init_weights(2, [2, 3, 1])
+    ws = [init_weights(2, [2, 3, 1]) for _ in 1:10]
+    xor_model_weights::Vector{BitArray}=[] 
+    epoch = 1
     while true
         x, y = gen_xor_data()
-        γ = model(x, w)[1]
-        if y == γ
-            println("found right combination!")
+        γs = predict(x, ws)
+        println(γs)
+        if [y] in γs
+            println("found right combination! epoch: $epoch")
+            w = ws[findfirst(isequal(y), map(x -> x[1], γs))]
             println(w)
+            xor_model_weights = w
             break
         end
-        w = init_weights(2, [2, 1])
+        ws = [init_weights(2, [2, 3, 1]) for _ in 1:10]
+        epoch += 1
     end
+
+    predictions = []
+    ground_truth = []
+    for _ in 1:10
+        x, y = gen_xor_data()
+        γ = model(x, xor_model_weights)[1]
+        push!(ground_truth, y)
+        push!(predictions, γ)
+    end
+
+    tp = length([i == j for (i, j) in zip(predictions, ground_truth) if i == true])
+    tn = length([i == j for (i, j) in zip(predictions, ground_truth) if i == false])
+
+    println("mean accuracy = $((tp + tn) / length(predictions))")
 end
 
 main()
